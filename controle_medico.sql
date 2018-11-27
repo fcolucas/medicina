700fa12fb9e7034c0d16748a3d0ae5991463b302
Lucas <fcolucaslima14@gmail.com>
2018-11-26 21:18:23 -0300

    Banco de dados atualizado

--- controle_medico.sql
+++ controle_medico.sql
@@ -3,24 +3,16 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 16-Nov-2018 às 04:47
+-- Generation Time: 27-Nov-2018 às 00:19
 -- Versão do servidor: 10.1.36-MariaDB
 -- versão do PHP: 7.2.11
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `controle_medico`
 --
+
+CREATE DATABASE controle_medico;
+USE controle_medico;
 
 -- --------------------------------------------------------
 
@@ -29,11 +21,47 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `medicamentos` (
-  `idmedicamentos` int(11) NOT NULL,
+  `idmedicamentos` int(11) NOT NULL AUTO_INCREMENT,
   `nomeMedicamento` varchar(50) NOT NULL,
-  `unidade` varchar(3) NOT NULL,
-  `quantidade` float NOT NULL
+  `unidade` varchar(4) NOT NULL,
+  `quantidade` float NOT NULL,
+  `descricao` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `medicamentos`
+--
+
+INSERT INTO `medicamentos` (`idmedicamentos`, `nomeMedicamento`, `unidade`, `quantidade`, descricao) VALUES
+("", 'Amoxilina', 'cap', 20, 'Antibiotico indicado para o tratamento de infecções bactecianas'),
+("", 'Sonrisal', 'cap', 2, "Antiácido e analgésico"),
+("", 'Viagra', 'cap', 20, "Tratamento da disfunção erétil"),
+("", 'AAS', 'cap', 14, "Antitérmico, analgésico e anti-inflamatório"),
+("", 'Aceticil', 'cap', 10, "Medicamento expectorante"),
+("", 'Acnase Gel', 'g', 1, "Tratamento de Acne"),
+("", 'Bactrim Comprimido', 'cap', 20, "Indicado para o tratamento de infecções"),
+("", 'Bactrim F Suspensão Oral', 'ml', 100, "Indicado para o tratamento de infecções"),
+("", 'Benalet', 'cap', 12, "Tratamento de sintomas, tais como tosse, irritação da garganta e faringite"),
+("", 'Benegrip', 'cap', 20, "Analgésico, antitérmico e descongestionante nasal"),
+("", 'Buscofem', 'cap', 10, 'Tratamento de febre e dores associadas a gripe'),
+("", 'Calcigenol', 'ml', 300, "Reposição de cálcio e de flúor"),
+("", 'Camomilina C', 'cap', 20, "Suplemento de vitaminas C e D3 que auxilia no alívio de sinais e sintomas associados com a primeira dentição"),
+("", 'Captopril', 'cap', 30, "Tratamento de hipertensão arterial"),
+("", 'Femynazol', 'g', 1, "Tratamento local de infecções"),
+("",'Ibuprofeno', 'ml', 30, 'Alivio temporário da febre e dores de leve a moderada'),
+("", 'Pamergan', 'cap', 10, "Indicado nas rinites alérgicas"),
+("", 'Paracetamol Solução Oral', 'ml', 15, "Redução da febre e para o alívio temporário de dores leves a moderadas"),
+("", 'Venalot', 'cap', 30, "Tratamento de problemas das veias e dos vasos linfáticos"),
+("",'Lacmax', 'ml', 120, 'Tratamento sintomatico da constipação intestinal'),
+("",'Gabapentina', 'cap', 30,'Tratamento da dor neuropatica'),
+("",'Magnazia', 'com', 30, 'Destinado para o tratamento da azia associada ao refluxo gástrico'),
+("",'Cefaliv', 'com', 12, 'Para o tratamento das crises de dor de cabeça'),
+("",'Ultrafer', 'ml', 10, 'Indicado no tratamento e preveção das anemias ferroprivas'),
+("",'Xefo', 'com', 30, 'Tratamento da dor lombar aguda e crônica'),
+("", 'Aceclofenaco', 'com', 16, 'Indicado para o tratamento de processos inflamatórios, dores de dente, dores agudas'),
+("", 'Advil', 'cap', 8, 'Analgesico, antinfamatório'),
+("", 'Acetazona', 'ml', 120, 'Eficiente no tratamento de infecções'),
+("", 'Dermalive', 'ml', 120, 'Proteção da pele danificada pelas intepéries diárias');
 
 -- --------------------------------------------------------
 
@@ -42,7 +70,7 @@ CREATE TABLE `medicamentos` (
 --
 
 CREATE TABLE `medicos` (
-  `idmedicos` int(11) NOT NULL,
+  `idmedicos` int(11) NOT NULL AUTO_INCREMENT,
   `nomeMedico` varchar(50) NOT NULL,
   `usuario` varchar(50) NOT NULL,
   `senha` varchar(32) NOT NULL
@@ -53,8 +81,8 @@ CREATE TABLE `medicos` (
 --
 
 INSERT INTO `medicos` (`idmedicos`, `nomeMedico`, `usuario`, `senha`) VALUES
-(1, 'Francisco Lucas', 'fcolucas', 'lucas1234'),
-(2, 'Juliana Cardoso', 'jucardoso', 'ju1234');
+("", 'Francisco Lucas', 'fcolucas', 'lucas1234'),
+("", 'Juliana Cardoso', 'jucardoso', 'ju1234');
 
 -- --------------------------------------------------------
 
@@ -63,7 +91,7 @@ INSERT INTO `medicos` (`idmedicos`, `nomeMedico`, `usuario`, `senha`) VALUES
 --
 
 CREATE TABLE `pacientes` (
-  `idpacientes` int(11) NOT NULL,
+  `idpacientes` int(11) NOT NULL AUTO_INCREMENT,
   `medicos_idmedicos` int(11) NOT NULL,
   `nomePaciente` varchar(50) NOT NULL,
   `dataNascimento` date NOT NULL,
@@ -73,25 +101,11 @@ CREATE TABLE `pacientes` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `prescricao`
---
-
-CREATE TABLE `prescricao` (
-  `receituarios_idreceituarios` int(11) NOT NULL,
-  `medicamentos_idmedicamentos` int(11) NOT NULL,
-  `dosagem` float NOT NULL,
-  `intervalo` int(11) NOT NULL,
-  `tipoIntervalo` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `receituarios`
 --
 
 CREATE TABLE `receituarios` (
-  `idreceituarios` int(11) NOT NULL,
+  `idreceituarios` int(11) NOT NULL AUTO_INCREMENT,
   `pacientes_idpacientes` int(11) NOT NULL,
   `medicos_idmedicos` int(11) NOT NULL,
   `data` date NOT NULL,
@@ -99,10 +113,21 @@ CREATE TABLE `receituarios` (
   `observacoes` text
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 --
--- Indexes for dumped tables
+-- Estrutura da tabela `prescricao`
 --
 
+CREATE TABLE `prescricao` (
+  `sequence` int(11) NOT NULL AUTO_INCREMENT,
+  `receituarios_idreceituarios` int(11) NOT NULL,
+  `medicamentos_idmedicamentos` int(11) NOT NULL,
+  `dosagem` float NOT NULL,
+  `intervalo` int(11) NOT NULL,
+  `tipoIntervalo` varchar(7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 --
 -- Indexes for table `medicamentos`
 --
@@ -126,6 +151,7 @@ ALTER TABLE `pacientes`
 -- Indexes for table `prescricao`
 --
 ALTER TABLE `prescricao`
+  ADD PRIMARY KEY (`sequence`),
   ADD KEY `receituarios_idreceituarios` (`receituarios_idreceituarios`),
   ADD KEY `medicamentos_idmedicamentos` (`medicamentos_idmedicamentos`);
 
@@ -136,38 +162,6 @@ ALTER TABLE `receituarios`
   ADD PRIMARY KEY (`idreceituarios`),
   ADD KEY `pacientes_idpacientes` (`pacientes_idpacientes`),
   ADD KEY `medicos_idmedicos` (`medicos_idmedicos`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `medicamentos`
---
-ALTER TABLE `medicamentos`
-  MODIFY `idmedicamentos` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `medicos`
---
-ALTER TABLE `medicos`
-  MODIFY `idmedicos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `pacientes`
---
-ALTER TABLE `pacientes`
-  MODIFY `idpacientes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `receituarios`
---
-ALTER TABLE `receituarios`
-  MODIFY `idreceituarios` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
 
 --
 -- Limitadores para a tabela `pacientes`
@@ -188,8 +182,5 @@ ALTER TABLE `prescricao`
 ALTER TABLE `receituarios`
   ADD CONSTRAINT `receituarios_ibfk_1` FOREIGN KEY (`pacientes_idpacientes`) REFERENCES `pacientes` (`idpacientes`),
   ADD CONSTRAINT `receituarios_ibfk_2` FOREIGN KEY (`medicos_idmedicos`) REFERENCES `medicos` (`idmedicos`);
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
